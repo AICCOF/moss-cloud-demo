39bfc2810efc70a591efbd1a17cab517b1f571a0
hpstream <huangpeng_2022@qq.com>
2023-07-10 17:46:31 +0800

    feat: 🎸 提交代码

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# moss-cloud-demo
-moss云demo
+# Moss云
+
+> 声明：此项目只发布于 GitHub，基于 MIT 协议，免费且作为Moss云API 开源学习使用。并且不会有任何形式的卖号、付费服务、讨论群、讨论组等行为。谨防受骗。
+
+本项目来源：[chatgpt-web](https://github.com/Chanzhaoyu/chatgpt-web)，作者很棒，多多给作者star
+
+![cover](./docs/c1.png)
+![cover2](./docs/c2.png)
+
+---
+
+## 使用说明
+
+```sh
+# 安装依赖
+pnpm install
+
+# 在.env 文件内 设置moss-key（在这里查看 https://openai.aihao123.cn/#/personalCenter/userInformation）
+
+# 启动项目
+npm run dev
+```
---
+++ src/api/index.ts
@@ -0,0 +1,65 @@
+import type { AxiosProgressEvent, GenericAbortSignal } from 'axios'
+import { post } from '@/utils/request'
+import { useAuthStore, useSettingStore } from '@/store'
+
+export function fetchChatAPI<T = any>(
+  prompt: string,
+  options?: { conversationId?: string; parentMessageId?: string },
+  signal?: GenericAbortSignal,
+) {
+  return post<T>({
+    url: '/chat',
+    data: { prompt, options },
+    signal,
+  })
+}
+
+export function fetchChatConfig<T = any>() {
+  return post<T>({
+    url: '/config',
+  })
+}
+
+export function fetchChatAPIProcess<T = any>(
+  params: {
+    model: string
+    prompt: string
+    onDownloadProgress?: (progressEvent: AxiosProgressEvent) => void },
+) {
+  const settingStore = useSettingStore()
+  const authStore = useAuthStore()
+
+  let data: Record<string, any> = {
+    model: params.model,
+    prompt: params.prompt,
+  }
+
+  if (authStore.isChatGPTAPI) {
+    data = {
+      ...data,
+      systemMessage: settingStore.systemMessage,
+      temperature: settingStore.temperature,
+      top_p: settingStore.top_p,
+    }
+  }
+
+  return post<T>({
+    url: 'https://api.aihao123.cn/moss/v1/completions',
+    data,
+    signal: params.signal,
+    onDownloadProgress: params.onDownloadProgress,
+  })
+}
+
+export function fetchSession<T>() {
+  return post<T>({
+    url: '/session',
+  })
+}
+
+export function fetchVerify<T>(token: string) {
+  return post<T>({
+    url: '/verify',
+    data: { token },
+  })
+}
